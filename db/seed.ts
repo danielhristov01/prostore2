9a5b4dff19807b14997ebff76261dffc4450d13f
unknown <danielhristov01@abv.bg>
2026-04-30 17:38:46 +0300

    second try

--- db/seed.ts
+++ db/seed.ts
@@ -5,7 +5,13 @@ import { prisma } from "./prisma";
 
 async function main() {
   await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
+
   await prisma.product.createMany({ data: sampleData.products });
+  await prisma.user.createMany({ data: sampleData.users });
   console.log("Database seeded successfully");
 }
 
