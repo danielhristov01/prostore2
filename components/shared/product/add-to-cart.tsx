e86d7c120c4f767a030e8861be71669903f9ac0a
unknown <danielhristov01@abv.bg>
2026-05-02 14:24:29 +0300

    Ui_transition

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -1,33 +1,81 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { addItemToCart } from "@/lib/actions/cart.action";
-import { CartItem } from "@/types";
-import { Plus } from "lucide-react";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.action";
+import { Cart, CartItem } from "@/types";
+import { Loader, Minus, Plus } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useTransition } from "react";
 import { toast } from "sonner";
 
-const AddToCart = ({ item }: { item: CartItem }) => {
+const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter();
+
+  const [isPending, startTransition] = useTransition();
+
   const handleAddToCart = async () => {
-    const res = await addItemToCart(item);
+    startTransition(async () => {
+      const res = await addItemToCart(item);
+      if (!res.success) {
+        toast.error(res.message);
+        return;
+      }
 
-    if (!res.success) {
-      toast.error(res.message);
-      return;
-    }
-
-    toast.success(res.message, {
-      action: {
-        label: "Go to cart",
-        onClick: () => router.push("/cart"),
-      },
+      toast.success(res.message, {
+        action: {
+          label: "Go to cart",
+          onClick: () => router.push("/cart"),
+        },
+      });
     });
   };
 
-  return (
+  const handleRemoveFromCart = async () => {
+    startTransition(async () => {
+      const res = await removeItemFromCart(item.productId);
+      if (!res.success) {
+        toast.error(res.message);
+        return;
+      }
+
+      toast.success(res.message, {
+        action: {
+          label: "Go to cart",
+          onClick: () => router.push("/cart"),
+        },
+      });
+    });
+  };
+
+  const existItem =
+    cart && cart.items.find((x) => x.productId === item.productId);
+
+  return existItem ? (
+    <div>
+      <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
+        {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Minus className="w-4 h-4 " />
+        )}
+      </Button>
+      <span className="px-2">{existItem.qty}</span>
+      <Button type="button" variant="outline" onClick={handleAddToCart}>
+        {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Plus className="w-4 h-4 " />
+        )}
+      </Button>
+    </div>
+  ) : (
     <Button className="w-full" type="button" onClick={handleAddToCart}>
-      <Plus /> Add To Cart
+      {isPending ? (
+        <Loader className="w-4 h-4 animate-spin" />
+      ) : (
+        <Plus className="w-4 h-4 " />
+      )}{" "}
+      Add To Cart
     </Button>
   );
 };
